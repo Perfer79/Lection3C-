7338b076285887298dfc752ce805996308995abe
Sergey <perfer79@gmail.com>
2022-05-24 15:47:46 +0700

    Третья лекция отдельный репозиторий

---
+++ Ex012_Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿// Вид 1. Ничего не принимают, **_ничего не возвращают_**.
+
+// void Method1()
+// {
+//     Console.WriteLine("Автор...");
+// }
+// Method1(); // вызов метода
+// //Method1 без скобок - ошибка
+
+// Вид2
+// void Method2(string msg)
+// {
+//      Console.WriteLine(msg);
+// }
+//Method2(msg: "Текст сообщения"); //вызов метода
+
+//вид2, вариант2
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count);
+    {
+        Console.WriteLine(msg);
+        i++; //Памятка: i++ = Инкремент, i-- = Декремент
+    }
+}
+Method21("Текст", 4);
